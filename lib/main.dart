35e7aa9a32fef0ec391b2adbcc3239639d620580
gold-roger33 <airudayip@gmail.com>
2024-02-17 00:34:21 +0530

    on tempbranch updated radiobutton with all previous functon

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:p_money_management/db/category/category_db.dart';
 import 'package:p_money_management/model/category/category_model.dart';
+import 'package:p_money_management/screens/add_transaction/transaction_screen.dart';
 import 'package:p_money_management/screens/home/home.dart';
 
 Future<void> main() async{
@@ -34,7 +35,10 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const screenhome()
+      home: const screenhome(),
+      routes: {
+        screentransaction.routname:(ctx)=>const screentransaction()
+      },
     );
   }
 }
